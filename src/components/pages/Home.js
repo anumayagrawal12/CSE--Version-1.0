41169270d5d20c343a0f3d90e8c660ea3cf8d922
anumayagrawal12 <103452273+anumayagrawal12@users.noreply.github.com>
2023-05-18 13:46:10 +0530

    4 files modified, 2 added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,13 +3,14 @@ import '../../App.css';
 import About from './About';
 import Mission from './Mission';
 import Header from './Header';
-
+import NewsAndAnnouncement from './NewsAndAnnouncement/NewsAndAnnouncement';
 export default function Home() {
   return (
     <div>
       <Header />
       <About />
       <Mission />
+      <NewsAndAnnouncement />
     </div>
   );
 }
